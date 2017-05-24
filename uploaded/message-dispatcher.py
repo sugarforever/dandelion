1591039e8927cf44b07b95c3c78fba5df2e59d42
William Yang <weili.yang@teamaol.com>
2017-05-24 10:21:51 +0100

    removed uploaded dir

--- uploaded/message-dispatcher.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-import os
-if __name__ == "__main__":
-	index = 1
-	while True:
-		print os.system("curl -d '%d: Hello Friday!' http://localhost:8080/api/notification/produce/" % index)
-		index = index + 1
